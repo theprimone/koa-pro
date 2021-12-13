98aee1d64f69dfd669230a53e80f881853a41463
theprimone <yuns.xie@qq.com>
2021-12-13 22:40:19 +0800

    feat: node debug mode

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # koa-pro
+
 🚀 Use Koa + TS like a Pro!
+
+## Feature
+
+- 热重载（Live Reload）
+- 开发环境默认为调试模式
--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,9 @@ const PORT = 3000;
 const app = new Koa();
 
 app.use(async (ctx) => {
+  console.log('start...');
   ctx.body = 'Hello World';
+  console.log('end...');
 });
 
 app.listen(PORT, () => {
